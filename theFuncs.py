1ac18e20c2e8b1d590274a74b319e54b37d66cc8
layoups <layounkarim@gmail.com>
2021-12-06 00:05:44 -0500

    data import

--- theFuncs.py
+++ theFuncs.py
@@ -23,3 +23,64 @@ from sklearn.exceptions import ConvergenceWarning
 
 from hyperopt import tpe, hp, fmin, STATUS_OK,Trials, space_eval
 from sklearn.datasets import fetch_openml
+
+def get_stock_factors_data(sedols=None):
+    df = pd.read_csv(
+        './data/rus1000_stocks_factors.csv', 
+        on_bad_lines='skip', 
+        header = 2, 
+        # nrows = 10000, 
+        low_memory=False, 
+        converters={'SEDOL': (lambda x: x[:6])},
+        parse_dates=['DATE'], 
+        index_col=[3, 2]
+    ).groupby(
+        ['Symbol', 'DATE']
+    ).fillna(
+        method='ffill'
+    ).sort_index()
+
+    return df
+
+def get_stock_return_data(sedols=None):
+    df = pd.read_csv(
+        "./data/cleaned_return_data_sc.csv", 
+        # nrows=3, 
+        parse_dates=['DATE'], 
+        index_col=0
+    ).fillna(method='ffill').fillna(0)
+
+    return df
+
+def get_benchmark_return_data():
+    df = pd.read_csv(
+        './data/Benchmark Returns.csv', 
+        on_bad_lines='skip', 
+        # nrows = 100, 
+        low_memory=False, 
+        parse_dates=['Date'], 
+        index_col=[0]
+    )
+    df.index.name = "DATE"
+
+    return df
+
+
+
+if __name__ == "__main__":
+    benchmark_returns = get_benchmark_return_data()
+
+    stock_returns = pd.read_csv(
+        "data/cleaned_return_data.csv", 
+        parse_dates=["DATE"],
+        index_col=[0]
+    )
+
+    factors = pd.read_csv(
+        "data/rus1000_stocks_factors_subset.csv",
+        parse_dates=["DATE"],
+        index_col=[0, 1]
+    )
+
+    print(benchmark_returns.head())
+    
